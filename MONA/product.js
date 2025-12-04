6c5b9fa96ae6617f54826bba14ef4d3a5a5a1e26
Christianonlay <onlaychristian99@gmail.com>
2025-12-04 19:34:14 +0800

    Added reserve page

--- MONA/product.js
+++ MONA/product.js
@@ -1,4 +1,3 @@
-// Read URL parameters
 const urlParams = new URLSearchParams(window.location.search);
 const name = urlParams.get("name");
 const price = urlParams.get("price");
@@ -9,3 +8,24 @@ document.getElementById("product-name").textContent = name;
 document.getElementById("product-img").src = img;
 document.getElementById("product-price").textContent = price;
 document.getElementById("total-price").textContent = price;
+
+
+// -------------------------------
+// RESERVE BUTTON FUNCTIONALITY
+// -------------------------------
+document.querySelector(".reserve-btn").addEventListener("click", () => {
+
+    let reservedItems = JSON.parse(localStorage.getItem("reservedItems")) || [];
+
+    reservedItems.push({
+        name: name,
+        price: Number(price),
+        img: img,
+        qty: 1
+    });
+
+    localStorage.setItem("reservedItems", JSON.stringify(reservedItems));
+
+    // Redirect to reserve page
+    window.location.href = "reserve.html";
+});
